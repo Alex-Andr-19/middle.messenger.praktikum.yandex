a266f70addbc96d6ff58406d0ec93b56a06c61a9
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-08-04 13:32:33 +0300

    Check testing 7

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
  
 const app = express();
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname));
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
 
-// app.get('*', function(req, res) {
-//   res.sendFile(__dirname + "/dist/index.html")
-// });
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html")
+});
  
 app.listen(PORT, () => {
   console.log(`Мой текст в логе после запуска ${PORT}!`);
